77797caf598151bc58736f64f418608a2c1bc13a
eliazarinelli <elia.zarinelli@gmail.com>
2017-01-02 09:40:38 +0000

    new table definitions in models

--- dana/models.py
+++ dana/models.py
@@ -1,43 +1,41 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String,  Float, BigInteger
+from sqlalchemy import Column, Integer, String,  Float, BigInteger, SmallInteger
 
 Base = declarative_base()
 
 
-class Order(Base):
-    __tablename__ = 'order'
+class Orders(Base):
+    __tablename__ = 'orders'
     id = Column(BigInteger, primary_key=True)
-    client = Column(String(250), nullable=False)
-    mgr = Column(String(250), nullable=False)
-    bkr = Column(String(250), nullable=False)
+    mgr_id = Column(String(250), nullable=False)
+    bkr_id = Column(String(250), nullable=False)
     symbol = Column(String(250), nullable=False)
-    side = Column(Integer, nullable=False)
-    date = Column(Integer, nullable=False)
-    start = Column(Integer, nullable=False)
-    end = Column(Integer, nullable=False)
-    volume = Column(Integer, nullable=False)
-    vwap = Column(Float, nullable=False)
-    volume_inf = Column(Integer, nullable=False)
-    vwap_inf = Column(Float, nullable=False)
-    cc = Column(Integer, nullable=False)
+    side = Column(SmallInteger, nullable=False)
+    date = Column(SmallInteger, nullable=False)
+    start_min = Column(SmallInteger, nullable=False)
+    end_min = Column(SmallInteger, nullable=False)
+    v_order = Column(Integer, nullable=False)
+    p_vwap = Column(Float, nullable=False)
+    n_trades = Column(SmallInteger, nullable=False)
 
 
 class DayInfo(Base):
     __tablename__ = 'dayinfo'
-    id = Column(BigInteger, primary_key=True)
-    volume = Column(Integer, nullable=False)
+    symbol = Column(String(250), primary_key=True)
+    date = Column(String(250), primary_key=True)
+    v_market = Column(Integer, nullable=False)
     p_vwap = Column(Float, nullable=False)
     p_open = Column(Float, nullable=False)
     p_close = Column(Float, nullable=False)
     p_high = Column(Float, nullable=False)
     p_low = Column(Float, nullable=False)
-    #volatility = Column(Float, nullable=False)
+    volatility = Column(Float, nullable=False)
 
 
 class PeriodInfo(Base):
     __tablename__ = 'periodinfo'
     id = Column(BigInteger, primary_key=True)
-    volume = Column(Integer, nullable=False)
+    v_market = Column(Integer, nullable=False)
     p_start = Column(Float, nullable=False)
     p_end = Column(Float, nullable=False)
-    vwap = Column(Float, nullable=False)
+    p_vwap = Column(Float, nullable=False)
