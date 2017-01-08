9e4e1edbe1d7725ef58ebe1ee3c2bbcd4a0d432b
eliazarinelli <elia.zarinelli@gmail.com>
2017-01-08 10:34:01 +0000

    added trade_extract_dict function and test

--- dana/parsers/parser_ans_functions.py
+++ dana/parsers/parser_ans_functions.py
@@ -1,1 +1,10 @@
 __author__ = 'eliazarinelli'
+
+def _trade_extract_dict(dict_in, name_mapping):
+
+	dict_out = {}
+
+	for k_out, k_in in name_mapping.items():
+		dict_out[k_out] = dict_in[k_in]
+
+	return dict_out
--- tests/t_parser_ans_functions.py
+++ tests/t_parser_ans_functions.py
@@ -1,1 +1,29 @@
 __author__ = 'eliazarinelli'
+
+import unittest
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import dana.parsers.parser_ans_functions as paf
+
+class Test_trade_extract_dict(unittest.TestCase):
+
+	def test_standard_case(self):
+
+		# actual input
+		dict_in = {
+			'key_in_0': 'val_in_0',
+			'key_in_1': 'val_in_1'
+		}
+
+		dict_mapping = {'key_out_1': 'key_in_1'}
+
+		# expected output
+		expected_output = {'key_out_1': 'val_in_1'}
+
+		# actual output
+		actual_output = paf._trade_extract_dict(dict_in, dict_mapping)
+
+		self.assertDictEqual(expected_output, actual_output)
