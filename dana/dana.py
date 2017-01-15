8a5bcfd1ff90dedc7c7a0af689bd03d6d3e31179
eliazarinelli <elia.zarinelli@gmail.com>
2017-01-15 16:46:22 +0000

    introduce get_day method in Dana

--- dana/dana.py
+++ dana/dana.py
@@ -6,23 +6,47 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import and_
 from sqlalchemy import func
-class Dana():
 
-	def __init__(self):
-		engine = create_engine('mysql+pymysql://' + USER_NAME + ':' + USER_PWD +
-							   '@' + HOST_NAME + '/' + DB_NAME)
-		Session = sessionmaker()
-		Session.configure(bind=engine)
-		self._session = Session()
 
-	def count_metaorders(self, symbol=None):
-		if symbol is None:
-			return self._session.query(Orders.id).count()
-		else:
-			return self._session.query(Orders.id).filter(Orders.symbol==symbol).count()
+class Dana(object):
 
-	def return_all(self):
-		return self._session.query(func.avg(Orders.date + DayInfo.v_market)).\
-			select_from(Orders).\
-			join(DayInfo, and_(Orders.symbol == DayInfo.symbol, Orders.date == DayInfo.date)).scalar()
+    def __init__(self, engine_url):
+
+        engine = create_engine(engine_url)
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        self._session = Session()
+
+    def count_orders(self, symbol=None):
+
+        if symbol is None:
+            return self._session.query(Orders.mgr).count()
+        else:
+            return self._session.query(Orders.mgr).filter(Orders.symbol==symbol).count()
+
+    def get_day(self, symbol=None, date=None):
+
+        output = []
+
+        record_list = self._session.query(Orders).\
+            filter(and_(Orders.symbol == symbol, Orders.date_exec == date))
+
+        for record in record_list:
+            order = {
+                'mgr': record.mgr,
+                'bkr': record.bkr,
+                'symbol': record.symbol,
+                'side': record.side,
+                'date_exec': record.date_exec,
+                'time_start': record.time_start,
+                'time_end': record.time_end,
+                'volume': record.volume,
+                'price': record.price,
+                'n_trades': record.n_trades
+            }
+
+            output.append(order)
+
+        return output
+
 
