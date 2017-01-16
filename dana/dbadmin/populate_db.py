39f933d845e43630e3eb64d2bc12a09b138281f0
eliazarinelli <elia.zarinelli@gmail.com>
2017-01-16 19:10:30 +0000

    removed session from the engine and added docstrings

--- dana/dbadmin/populate_db.py
+++ dana/dbadmin/populate_db.py
@@ -7,50 +7,47 @@ sys.path.insert(0, os.path.abspath('../..'))
 from dana.models import Base, Orders
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.exc import IntegrityError
 
 import pickle
 import time
 
 
-DBSession = scoped_session(sessionmaker())
-engine = None
+def _init_sqlalchemy(engine_url_in):
+
+    """ Create an instance of the engine specified by the input url """
+
+    return create_engine(engine_url_in, echo=False)
 
 
-def _init_sqlalchemy(engine_url):
+def _clean_db(engine_in):
 
-    global engine
-    engine = create_engine(engine_url, echo=False)
-    DBSession.remove()
-    DBSession.configure(bind=engine, autoflush=False, expire_on_commit=False)
-
-
-def _clean_db():
-
-    global engine
+    """ Drop the tables of the Base class and create new ones"""
 
     # remove all tables in the database
-    Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine_in)
 
     # create all tables in the database defined in Base
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine_in)
 
 
 def _read_orders(path_stage, name_pickle):
 
-    list_orders = pickle.load(open('/'.join([path_stage, name_pickle]), "rb"))
-    return list_orders
+    """ Read the orders stored in a pickle file """
+
+    return pickle.load(open('/'.join([path_stage, name_pickle]), "rb"))
 
 
-def _populate_db(list_orders):
+def _populate_db(engine_in, list_orders_in):
+
+    """ Populate the database with the input orders """
 
     t_0 = time.time()
 
     try:
-        engine.execute(Orders.__table__.insert(), list_orders)
+        engine_in.execute(Orders.__table__.insert(), list_orders_in)
         print(
-            "Added " + str(len(list_orders)) +
+            "Added " + str(len(list_orders_in)) +
             " records in " + str(time.time() - t_0) + " secs")
 
     except IntegrityError:
@@ -60,28 +57,32 @@ def _populate_db(list_orders):
 
 if __name__ == '__main__':
 
-    bool_clean_db = True
-
-    path_stage = os.path.relpath('../../db/stage')
-
-    name_pickle = 'example.p'
-
+    # database configurations
     db_path = os.path.abspath('../../db/db')
-    if not os.path.exists(db_path):
-        os.makedirs(db_path)
-
     db_name = 'example.db'
     engine_url = '/'.join(['sqlite:///', db_path, db_name])
 
-    _init_sqlalchemy(engine_url)
+    # create the folder where the db is stored
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
 
+    # create the engine
+    engine = _init_sqlalchemy(engine_url)
+
+    # clean the db
+    # set the flag to True if you want to clean
+    bool_clean_db = True
     if bool_clean_db:
-        _clean_db()
+        _clean_db(engine)
         print('Cleaned and initialised db in:')
         print(engine_url)
 
-    list_orders = _read_orders(path_stage, name_pickle)
+    # pickle configurations
+    path_stage = os.path.relpath('../../db/stage')
+    name_pickle = 'example.p'
 
-    _populate_db(list_orders)
+    # read the orders in the pickle file and store in the db
+    list_orders = _read_orders(path_stage, name_pickle)
+    _populate_db(engine, list_orders)
 
 
