42de9f5509fe93cf8aa708f1e48b2118f3c47ebf
eliazarinelli <elia.zarinelli@gmail.com>
2017-03-01 19:06:53 +0000

    refactoring: splitting dana in orderreservoir and hts

--- dana/dana.py
+++ dana/dana.py
@@ -9,15 +9,134 @@ ORDERS_FIELDS = set(['mgr', 'bkr', 'symbol', 'sign', 'date',
                      'min_start', 'min_end', 'volume', 'price', 'ntrades', 'is_ok'])
 
 
-class Dana(object):
+class OrdersReservoir(object):
 
-    def __init__(self, engine_url, db_ts_name, db_orders_name):
-
-        self._db_ts_name = db_ts_name
-        self._db_orders_name = db_orders_name
+    def __init__(self, engine_url, db_name):
         self._client = MongoClient(engine_url)
-        self._db_ts = self._client[db_ts_name]
-        self._db_orders = self._client[db_orders_name]
+        self._db_name = db_name
+        self._db_orders = self._client[self._db_name]
+
+    def empty_reservoir(self):
+
+        """ Drop the orders database """
+
+        self._client.drop_database(self._db_name)
+
+    def insert_orders(self, orders):
+
+        """ Insert orders in the orders db """
+
+        if set(orders[0].keys()) != ORDERS_FIELDS:
+            raise ValueError('Wrong format of input in orders: dict keys should be: ' + ', '.join(ORDERS_FIELDS))
+
+        # get the orders collection
+        collection_orders = self._db_orders.get_collection('orders')
+
+        # insert input orders
+        collection_orders.insert_many(orders)
+
+    def get_orders(self, symbol=None, date=None):
+
+        """ Get orders corresponding to symbol and date """
+
+        # create filter for symbol and dates
+        filter_order = {}
+        if date is not None:
+            filter_order['date'] = date
+        if symbol is not None:
+            filter_order['symbol'] = symbol
+
+        # get the collection of orders
+        collection_orders = self._db_orders['orders']
+
+        # filtering on the date and symbol
+        cc = collection_orders.find(filter_order)
+        return list(cc)
+
+    def get_symbols(self, date=None):
+
+        # create filter for date
+        filter_order = {}
+        if date is not None:
+            filter_order['date'] = date
+
+        # get the collection of orders
+        collection_orders = self._db_orders['orders']
+
+        symbols = collection_orders.find(filter_order, {'symbol': 1}).distinct('symbol')
+        symbols_list = list(symbols)
+        symbols_list.sort()
+        return symbols_list
+
+    def get_dates(self, symbol=None):
+
+        # create filter symbol
+        filter_order = {}
+        if symbol is not None:
+            filter_order['symbol'] = symbol
+
+        # get the collection of orders
+        collection_orders = self._db_orders['orders']
+
+        dates = collection_orders.find(filter_order, {'date': 1}).distinct('date')
+        dates_list = list(dates)
+        dates_list.sort()
+        return dates_list
+
+    def add_market_info(self, order_id, candle_day, candle_period):
+
+        # get the collection of the orders
+        collection_orders = self._db_orders['orders']
+
+        try:
+            # get the order
+            order = collection_orders.find({'_id': order_id})[0]
+
+            # participation rate day
+            pr_day = float(order['volume'])/float(candle_day['volume'])
+
+            # participation rate period
+            pr_period = float(order['volume'])/float(candle_period['volume'])
+
+            # duration volume time
+            duration_vol = float(candle_period['volume'])/float(candle_day['volume'])
+
+            # duration physical time
+            duration_ph = order['min_end'] - order['min_start']
+
+            # daily volatility
+            volatility_day = float(candle_day['high']-candle_day['low'])/float(candle_day['open'])
+
+            # temporary impact
+            impact_end = order['sign'] * np.log(float(candle_period['close'])/candle_period['open'])/volatility_day
+
+            # impact vwap
+            impact_vwap = order['sign'] * np.log(float(order['price'])/candle_period['open'])/volatility_day
+
+            collection_orders.update_one(
+                {'_id': order['_id']},
+                {'$set': {
+                    'is_ok': True,
+                    'candle_period': candle_period,
+                    'candle_day': candle_day,
+                    'pr_day': pr_day,
+                    'pr_period': pr_period,
+                    'duration_vol': duration_vol,
+                    'duration_ph': duration_ph,
+                    'volatility_day': volatility_day,
+                    'impact_end': impact_end,
+                    'impact_vwap': impact_vwap,
+                }})
+        except:
+            pass
+
+
+class Hts(object):
+
+    def __init__(self, engine_url, db_name):
+        self._client = MongoClient(engine_url)
+        self._db_name = db_name
+        self._db_hts = self._client[self._db_name]
 
     def drop_ts(self, symbol=None):
 
@@ -30,9 +149,9 @@ class Dana(object):
         """
 
         if symbol is None:
-            self._client.drop_database(self._db_ts_name)
+            self._client.drop_database(self._db_name)
         else:
-            self._db_ts.drop_collection(symbol)
+            self._db_hts.drop_collection(symbol)
 
     def insert_ts(self, symbol, ts):
 
@@ -49,7 +168,7 @@ class Dana(object):
         if set(ts[0].keys()) != TS_FIELDS:
             raise ValueError('Wrong format of input in ts: dict keys should be: ' + ', '.join(TS_FIELDS))
 
-        collection_symbol = self._db_ts.get_collection(symbol)
+        collection_symbol = self._db_hts.get_collection(symbol)
         collection_symbol.insert_many(ts)
 
     def get_ts(self, symbol, date, min_start=0, min_end=N_MINS_DAY):
@@ -57,7 +176,7 @@ class Dana(object):
         """ Get the time series corresponding to a symbol and date """
 
         # get the collection corresponding to the symbol
-        collection_symbol = self._db_ts.get_collection(symbol)
+        collection_symbol = self._db_hts.get_collection(symbol)
 
         # get the cursor filtering on the date, min_start and min_end
         # sort by the minutes
@@ -71,7 +190,7 @@ class Dana(object):
     def ts_index(self, symbol):
 
         # get the collection corresponding to the symbol
-        collection_symbol = self._db_ts.get_collection(symbol)
+        collection_symbol = self._db_hts.get_collection(symbol)
         collection_symbol.create_index([('date', pymongo.ASCENDING)])
 
     def get_candle(self, symbol, date, min_start=0, min_end=N_MINS_DAY):
@@ -79,7 +198,7 @@ class Dana(object):
         """ Get the candle corresponding to a symbol and date """
 
         # get the collection corresponding to the symbol
-        collection_symbol = self._db_ts.get_collection(symbol)
+        collection_symbol = self._db_hts.get_collection(symbol)
 
         # get the cursor filtering on the date, min_start and min_end
         # sort by the minutes
@@ -109,124 +228,6 @@ class Dana(object):
             del candle['pv']
             return candle
 
-    def drop_orders(self):
-
-        """ Drop the orders database """
-
-        self._client.drop_database(self._db_orders_name)
-
-    def insert_orders(self, orders):
-
-        """ Insert orders in the orders db """
-
-        if set(orders[0].keys()) != ORDERS_FIELDS:
-            raise ValueError('Wrong format of input in orders: dict keys should be: ' + ', '.join(ORDERS_FIELDS))
-
-        # get the orders collection
-        collection_orders = self._db_orders.get_collection('orders')
-
-        # insert input orders
-        collection_orders.insert_many(orders)
-
-    def get_orders(self, symbol, date, start_min=0, start_max=N_MINS_DAY, end_min=0, end_max=N_MINS_DAY):
-
-        """ Get orders corresponding to symbol and date """
-
-        # get the collection of orders
-        collection_orders = self._db_orders['orders']
-
-        # filtering on the date and symbol
-        # filtering on min_start and min_end
-        # do not return id
-        cc = collection_orders.find({'date': date,
-                                     'symbol': symbol,
-                                     'min_start': {'$gte': start_min, '$lte': start_max},
-                                     'min_end': {'$gte': end_min, '$lte': end_max},
-                                     },
-                                    {"_id": 0})
-        return list(cc)
-
-    def assign_candle_orders(self, symbol, date):
-
-        """ Assign and store in the db for each order corresponding
-        to symbol and date the day and period candle """
-
-
-        # get the collection of the orders
-        collection_orders = self._db_orders['orders']
-
-        # get the cursor of the order corresponding to symbol and date
-        cursor_orders = collection_orders.find(
-            {'date': date,
-             'symbol': symbol})
-
-        # get the day_candle
-        candle_day = self.get_candle(symbol=symbol, date=date)
-
-        for order in cursor_orders:
-
-            if not order['is_ok']:
-                try:
-                    # get the candle period
-                    candle_period = self.get_candle(symbol, date, min_start=order['min_start'], min_end=order['min_end'])
-
-                    # participation rate day
-                    pr_day = float(order['volume'])/float(candle_day['volume'])
-
-                    # participation rate period
-                    pr_period = float(order['volume'])/float(candle_period['volume'])
-
-                    # duration volume time
-                    duration_vol = float(candle_period['volume'])/float(candle_day['volume'])
-
-                    # duration physical time
-                    duration_ph = order['min_end'] - order['min_start']
-
-                    # daily volatility
-                    volatility_day = float(candle_day['high']-candle_day['low'])/float(candle_day['open'])
-
-                    # temporary impact
-                    impact_end = order['sign'] * np.log(float(candle_period['close'])/candle_period['open'])/volatility_day
-
-                    # impact vwap
-                    impact_vwap = order['sign'] * np.log(float(order['price'])/candle_period['open'])/volatility_day
-
-                    collection_orders.update_one(
-                        {'_id': order['_id']},
-                        {'$set': {
-                            'is_ok': True,
-                            'candle_period': candle_period,
-                            'candle_day': candle_day,
-                            'pr_day': pr_day,
-                            'pr_period': pr_period,
-                            'duration_vol': duration_vol,
-                            'duration_ph': duration_ph,
-                            'volatility_day': volatility_day,
-                            'impact_end': impact_end,
-                            'impact_vwap': impact_vwap,
-                        }})
-                except:
-                    pass
-
-    def get_available_symbol(self):
-
-        # get the collection of orders
-        collection_orders = self._db_orders['orders']
-
-        symbols = collection_orders.find({}, {'symbol': 1}).distinct('symbol')
-        symbols_list = list(symbols)
-        symbols_list.sort()
-        return symbols_list
-
-    def get_available_dates(self):
-
-        # get the collection of orders
-        collection_orders = self._db_orders['orders']
-
-        dates = collection_orders.find({}, {'date': 1}).distinct('date')
-        dates_list = list(dates)
-        dates_list.sort()
-        return dates_list
 
 # ##############################################################################
 
@@ -286,34 +287,44 @@ if __name__ == '__main__':
     market_open = 1
     market_close = 2**10
     v_max = 1
-    n_orders = 2**8
-    n_dates = 2**6
+    n_orders = 2**0
+    n_dates = 2**0
 
-#    api = Dana(engine_url='mongodb://192.168.1.12:27017/', db_ts_name='ts_example', db_orders_name='orders_example')
-    api = Dana(engine_url='mongodb://localhost:27017/', db_ts_name='ts_example', db_orders_name='orders_example')
-
-    print('drop db')
-    t0 = time.time()
-    api.drop_ts()
-    api.drop_orders()
-    print(time.time() - t0)
+    reservoir = OrdersReservoir(engine_url='mongodb://localhost:27017/', db_name='orders_example')
+    hts = Hts(engine_url='mongodb://localhost:27017/', db_name='ts_example')
 
     print('Insert ' + str(n_dates) + ' ts:')
     t0 = time.time()
+    hts.drop_ts()
     for i in range(date, date+n_dates):
         ts = generate_ts(date=i, start=market_open, end=market_close, v_max=v_max)
-        api.insert_ts(symbol, ts)
-    api.ts_index(symbol=symbol)
+        print(len(ts))
+        hts.insert_ts(symbol, ts)
+    hts.ts_index(symbol=symbol)
     print(time.time() - t0)
+
+    cc = hts.get_candle(symbol=symbol, date=date, min_start=market_open, min_end=market_close)
 
     print('Insert ' + str(n_orders) + ' orders:')
     t0 = time.time()
-    orders_in = [generate_order(symbol=symbol, date=date, min_start=market_open, min_end=market_close) for i in range(n_orders)]
-    api.insert_orders(orders_in)
+    reservoir.empty_reservoir()
+    orders_in = []
+    orders_in.append(generate_order(symbol=symbol, date=date, min_start=market_open, min_end=market_close))
+    orders_in.append(generate_order(symbol=symbol, date=date+1, min_start=market_open, min_end=market_close))
+    orders_in.append(generate_order(symbol='MSFT', date=date, min_start=market_open, min_end=market_close))
+    orders_in.append(generate_order(symbol='MSFT', date=date+1, min_start=market_open, min_end=market_close))
+
+    reservoir.insert_orders(orders_in)
     print(time.time() - t0)
 
-    print('Insert candles:')
-    t0 = time.time()
-    api.assign_candle_orders(symbol=symbol, date=date)
-    print(time.time() - t0)
+    order_list = reservoir.get_orders()
+    print(len(order_list))
+    for order in order_list:
+        reservoir.add_market_info(order['_id'], candle_day=cc, candle_period=cc)
+
+    list_dates = reservoir.get_dates(symbol=symbol)
+    print(list_dates)
+
+    list_symbols = reservoir.get_symbols()
+    print(list_symbols)
 
